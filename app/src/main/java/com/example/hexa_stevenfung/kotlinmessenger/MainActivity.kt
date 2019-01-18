bfa00f5fc8c899306cf68ce5b0fcaede36e0c39c
Hexa-Steven.Fung <steven.fung@hexainitiative.com>
2019-01-18 18:04:51 +0800

    Installing Firebase Auth & Register User

--- app/src/main/java/com/example/hexa_stevenfung/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/hexa_stevenfung/kotlinmessenger/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.hexa_stevenfung.kotlinmessenger
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,14 @@ class MainActivity : AppCompatActivity() {
             val username = username_edittext_register.text.toString()
             val email = email_edittext_register.text.toString()
             val password = password_edittext_register.text.toString()
+
+            //Firebase Authentication to register
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+                }
         }
 
         already_have_account_text_view.setOnClickListener {
