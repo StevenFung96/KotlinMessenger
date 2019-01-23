5f13268fa4900648aadb0f06dddcd5d5a358b5c4
Hexa-Steven.Fung <steven.fung@hexainitiative.com>
2019-01-23 11:23:49 +0800

    -Add error handling for Firebase Auth
    -Create rounded corners UI

--- app/src/main/java/com/example/hexa_stevenfung/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/hexa_stevenfung/kotlinmessenger/LoginActivity.kt
@@ -2,6 +2,9 @@ package com.example.hexa_stevenfung.kotlinmessenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,6 +15,21 @@ class LoginActivity : AppCompatActivity() {
         login_button_login.setOnClickListener {
             val email = email_edittext_login.text.toString()
             val password = password_edittext_login.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    Log.d("Main", "Successfully login user with uid: ${it.result?.user?.uid}")
+                    Toast.makeText(
+                        this,
+                        "Successfully login user with uid: ${it.result?.user?.uid}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .addOnFailureListener {
+                    Log.d("Main", "Falied to login user account: ${it.message}")
+                    Toast.makeText(this, "Falied to login user account: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
         }
 
         back_to_register_textview.setOnClickListener {
--- app/src/main/java/com/example/hexa_stevenfung/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/example/hexa_stevenfung/kotlinmessenger/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,22 +15,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         register_button_register.setOnClickListener {
-            val username = username_edittext_register.text.toString()
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            //Firebase Authentication to register
-            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (!it.isSuccessful) return@addOnCompleteListener
-
-                    Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
-                }
+            performRegister()
         }
 
         already_have_account_text_view.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val username = username_edittext_register.text.toString()
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //Firebase Authentication to register
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+                Toast.makeText(this, "Successfully created user with uid: ${it.result?.user?.uid}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Falied to create user: ${it.message}")
+                Toast.makeText(this, "Falied to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
